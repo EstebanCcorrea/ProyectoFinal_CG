e6f0245d5ae39abda359e178c079f9fa74e4297c
Salax-18 <katheduque08@gmail.com>
2025-11-08 06:56:29 -0500

    escenaLava
    se añade un sistema de particulas

--- ProyectoFinal_CG/Assets/GAME/Scripts/LavaAnimation.cs
+++ ProyectoFinal_CG/Assets/GAME/Scripts/LavaAnimation.cs
@@ -3,9 +3,7 @@ using UnityEngine;
 public class LavaAnimation : MonoBehaviour
 {
     [Header("Velocidad del flujo")]
-    public float scrollX = 0.25f;
-    public float scrollY = 0.1f;
-
+    public float scrollSpeed = 0.2f; 
     private Renderer rend;
 
     void Start()
@@ -15,15 +13,14 @@ public class LavaAnimation : MonoBehaviour
 
     void Update()
     {
-        
-        float offsetX = Time.time * scrollX;
-        float offsetY = Time.time * scrollY;
-        rend.material.SetTextureOffset("_BaseMap", new Vector2(offsetX, offsetY));
 
-        // Si el material usa emisión, se anima también el mapa de emisión
+        float offset = Time.time * scrollSpeed;
+        Vector2 newOffset = new Vector2(offset, 0); // movimiento solo en X
+
+        // Si el material usa emisión
         if (rend.material.IsKeywordEnabled("_EMISSION"))
         {
-            rend.material.SetTextureOffset("_EmissionMap", new Vector2(offsetX * 0.5f, offsetY * 0.5f));
+            rend.material.SetTextureOffset("_EmissionMap", newOffset * 0.5f);
         }
     }
 }
